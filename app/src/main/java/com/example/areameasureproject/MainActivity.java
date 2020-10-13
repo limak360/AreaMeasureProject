1b834d28ab2bad2d7005a4d0806a92a66688a58c
Kamil Jacko <limak360@gmail.com>
2020-10-13 22:33:03 +0200

    Changes: - provided google maps api key - permissions request handling - isGPSEnabled method - a bit of getting coordinates, but doesnt work yet

--- app/src/main/java/com/example/areameasureproject/MainActivity.java
+++ app/src/main/java/com/example/areameasureproject/MainActivity.java
@@ -1,99 +1,176 @@
 package com.example.areameasureproject;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-    private static final int REQUEST_CODE_PERMISSION = 1;
+import static com.example.areameasureproject.Constants.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
+import static com.example.areameasureproject.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
     private static final String TAG = "MainActivity";
-    String[] mPermission = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
 
-    private TextView exitBtn;
-    private LocationManager manager;
-    private String locationProvider;
+    private boolean mLocationPermissionGranted = false;
+    private FusedLocationProviderClient mfusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        exitBtn = (TextView) findViewById(R.id.exitTextView);
-
-        Criteria kryteria = new Criteria();
-        locationProvider = manager.getBestProvider(kryteria, true);
-
-        //wyswietl("\nTwoja Lokalizacja:");
-
-        assert locationProvider != null;
-        @SuppressLint("MissingPermission") Location location = manager.getLastKnownLocation(locationProvider);
-//        wyrzucLokacje(location);
-
-        wyswietl("\nTwoja Lokalizacja:");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
-                        PackageManager.PERMISSION_GRANTED) {
+        if (isGPSEnabled()) {
+            if (mLocationPermissionGranted) {
+                Toast.makeText(this, "UDALO SIE", Toast.LENGTH_SHORT).show();
+                getLastKnownLocation();
+            } else {
+                getLocationPermission();
+            }
+        }
+    }
+
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation: called.");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG, "getLastKnownLocation: CHUJA DZIALA");
             return;
         }
-        manager.requestLocationUpdates(locationProvider, 150, 1, this);
+        mfusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    Location location = task.getResult();
+                    Log.d(TAG, "onComplete: latitude:" + location.getLatitude());
+                    Log.d(TAG, "onComplete: longitude:" + location.getLongitude());
+                }
+            }
+        });
+    }
+
+    public boolean isGPSEnabled() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        manager.removeUpdates(this);
-    }
-
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-        wyrzucLokacje(location);
-    }
-
-    private void wyswietl(String string) {
-        exitBtn.append(string);
-    }
-
-    private void wyrzucLokacje(Location location) {
-        if (location == null)
-            wyswietl("\nLokacja niedostepna");
-        else
-            wyswietl("\n" + location.toString());
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.d(TAG, "onRequestPermissionResult: called.");
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                if (mLocationPermissionGranted) {
+                    Toast.makeText(this, "UDALO SIE", Toast.LENGTH_SHORT).show();
+                    getLastKnownLocation();
+                } else {
+                    getLocationPermission();
+                }
+            }
         }
     }
 
+    /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+            Toast.makeText(this, "UDALO SIE", Toast.LENGTH_SHORT).show();
+            getLastKnownLocation();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+//    private void verifyPermissions() {
+//        Log.d(TAG, "verifyPermissions: Checking Permissions.");
+//
+//        int permissionCallPhone = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+//        if (permissionCallPhone != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(
+//                    MainActivity.this,
+//                    CALL_PERMISSIONS,
+//                    1
+//            );
+//        }
+//
+//        int permissionExternalMemory = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        if (permissionExternalMemory != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(
+//                    MainActivity.this,
+//                    STORAGE_PERMISSIONS,
+//                    1
+//            );
+//        }
+//    }
 }
